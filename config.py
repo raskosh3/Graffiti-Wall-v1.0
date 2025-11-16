c3c52fac2fae7e019a63fe0f4473a9c0d5905d2d
raskosh3 <163674380+raskosh3@users.noreply.github.com>
2025-11-17 02:23:16 +0300

    Modify default values in Config class
    Updated default values for environment variables in Config class.

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
 from dataclasses import dataclass
 
-
 @dataclass
 class Config:
-    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "ТВОЙ_ТОКЕН")
-    MONGODB_URL: str = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
-    WEBAPP_URL: str = os.getenv("WEBAPP_URL", "https://your-app.onrender.com")
-
-
+    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
+    MONGODB_URL: str = os.getenv("MONGODB_URL", "")
+    WEBAPP_URL: str = os.getenv("WEBAPP_URL", "")
+    
 config = Config()
